19d44405d4b150c4f082418df35ea1e7bd9be485
Abdelhamid Aiche <62718186+aicheh@users.noreply.github.com>
2020-03-26 23:43:17 +0100

    Create INFO-Dag-and-Databases.ps1

---
+++ INFO-Dag-and-Databases.ps1
@@ -0,0 +1,24 @@
+# Création d'un DAG ipless :
+
+$DagName       = "DAG-E2016" 
+$WitnessServer = "SERVEUR_TEMOIN"
+$WitnessDir    = "C:\DAG-FSW"  
+
+New-DatabaseAvailabilityGroup -Name $DagName -DatabaseAvailabilityGroupIPAddresses ([System.Net.IPAddress]::None) -WitnessServer $WitnessServer –WitnessDirectory $WitnessDir
+Add-DatabaseAvailabilityGroupServer -identity $DagName –MailboxServer "SERVEUR_EXC01"
+Add-DatabaseAvailabilityGroupServer -identity $DagName –MailboxServer "SERVEUR_EXC02"
+
+Note : Contrairement au DAG avec adresse ip, il n'est pas nécessaire ici de créer et préparer un objet ordinateur CNO.
+
+
+# Création d'une liste de bases de données depuis un fichier CSV (séparateur ;)
+# Préparer d'abord un fichier CSV avec 3 colonnes : Name, EdbFilePath, LogFolderPath et lister y la liste des bases de données à créer.
+
+$Databases     = Import-Csv ".\databases.csv" -delimiter ";"
+
+$Databases | % {
+      write-host "Création de la base de données : " $_.Name
+      New-Mailboxdatabase -server "KBA-EXC-IN-P01" -name $_.Name  -Edbfilepath $_.EdbFilePath -logFolderPath $_.LogFolderPath -ErrorAction SilentlyContinue  
+      Sleep -s 10 
+}
+
